17229f30cf257075d1f24d5ab63d7e0291470314
Claude Aldric <cvaldric@gmail.com>
2020-04-02 00:38:41 -0400

    Add total population denominator to S and I compartments

--- model.py
+++ model.py
@@ -1,50 +1,52 @@
 # Import required modules
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
 from os.path import join
 from gekko import GEKKO
 
+# Data variables
+breakpoint()
+infected_data = pd.read_excel(join('data', 'source.xlsx'), sheet_name='infected_state')
+infected_data = infected_data.groupby('date')['infected_count'].sum()
+infected_data = infected_data.sort_index()
+infected_data = np.array(infected_data)
 
-def main():
-    # Data variables
-    infected_data = pd.read_excel(join('data', 'source.xlsx'), sheet_name='infected_state')
-    infected_data = infected_data.groupby('date')['infected_count'].sum()
-    infected_data = infected_data.sort_index()
-    infected_log_10 = np.log10(np.array(infected_data))
+# Model
+model = GEKKO()
+model.time = np.array(range(len(infected_data)))
 
-    # Model
-    model = GEKKO()
-    model.time = np.array(range(len(infected_data)))
-    # model.time = np.array(infected_data.index)
+# Parameters
+total_us_population = 328_239_523
+initial_infected = 1
 
-    # Parameters
-    total_us_population = 328_239_523
-    initial_infected = 1
+recovery_rate = 1 / 19.6
+infection_rate = model.FV(value=0.1, lb=0, ub=1)
+infection_rate.STATUS = 1
 
-    recovery_rate = 1 / 19.6
-    infection_rate = model.MV(lb=0)
-    infection_rate.STATUS = 1
+# Variables
+susceptible = model.Var(value=total_us_population)
+infected = model.Var(value=initial_infected)
+recovered = model.Var(value=0)
 
-    # Variables
-    susceptible = model.Var(value=total_us_population)
-    infected = model.Var(value=initial_infected)
-    recovered = model.Var(value=0)
+observed = model.Param(value=infected_data)
 
-    # System of ordinary differential equations: SIR model
-    model.Equation(susceptible.dt() == -infection_rate * infected * susceptible)
-    model.Equation(infected.dt() == infection_rate * infected * susceptible - recovery_rate * infected)
-    model.Equation(recovered.dt() == recovery_rate * infected)
+# System of ordinary differential equations: SIR model
+model.Equation(susceptible.dt() == -infection_rate * infected * susceptible / total_us_population)
+model.Equation(infected.dt() == infection_rate * infected * susceptible / total_us_population - recovery_rate * infected)
+model.Equation(recovered.dt() == recovery_rate * infected)
 
-    # Objective function
-    model.Obj(mean_squared_error(infected_log_10, np.log10(infected)))
+# Objective function
+model.Obj((observed - infected) ** 2)
 
-    model.options.IMODE = 6
-    model.solve()
+model.options.IMODE = 6
+model.solve()
 
-
-def mean_squared_error(observed, predicted):
-    return sum((observed - predicted) ** 2) / len(observed)
-
-
-if __name__ == '__main__':
-    main()
+plt.figure(1)
+plt.plot(model.time, infected.value, 'r:', label='infected')
+plt.plot(model.time, observed.value, 'bo', label='observed')
+plt.legend(loc='best')
+plt.xlabel('time (days)')
+plt.ylabel('infected count')
+plt.show()
