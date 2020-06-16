7339227a03deb769dba1b0d30574e8ba73ebff34
caldric <cvaldric@gmail.com>
2020-06-16 17:59:27 -0400

    Change API source

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,24 @@
 // URLs
-const timeSeriesURL = 'https://raw.githubusercontent.com/CSSEGISandData/'
-  + 'COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/';
-const confirmedURL = timeSeriesURL + 'time_series_covid19_confirmed_US.csv';
-const deathsURL = timeSeriesURL + 'time_series_covid19_deaths_US.csv';
+const confirmedCurrent = 'https://covidtracking.com/api/v1/states/current.json';
 
 // Data variables
-const latestDate = '6/15/20';
 const data = {};
 
 // Functions
-const getData = (url, category) => Papa.parse(url, {
-  download: true,
-  header: true,
-  complete: csv => {
-    data[category] = csv.data;
-  }
+const getData = () => $.ajax({url: confirmedCurrent}).then((theData) => {
+  console.log(theData);
+  console.log(theData instanceof Array);
+  console.log(getCount(theData));
 });
 
 const getCount = (arr) => {
   let total = 0;
   for (let row of arr) {
-    if (typeof row[latestDate] == 'string') {
-      let confirmedCount = parseInt(row[latestDate]);
-      total += confirmedCount;
-    }
+    total += row.positive;
   }
   return total;
 };
 
 $(() => {
-  getData(confirmedURL, 'confirmed');
-  getData(deathsURL, 'deaths');
-
-  console.log(data.confirmed);
-  console.log(getCount(data.confirmed));
+  getData();
 });
