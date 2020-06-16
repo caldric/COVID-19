99583e1b31cbe1ff1c2d2a481cf279b13cd77c71
caldric <cvaldric@gmail.com>
2020-06-16 18:09:17 -0400

    Generalize getCount()

--- scripts/app.js
+++ scripts/app.js
@@ -8,12 +8,12 @@ const data = {};
 const getData = () => $.ajax({url: confirmedCurrent}).then((theData) => {
   console.log(theData);
   console.log(theData instanceof Array);
-  console.log(getCount(theData));
+  console.log(getCount(theData, 'positive'));
 });
 
-const getCount = (arr) => {
+const getCount = (arr, attr) => {
   let total = 0;
-  arr.forEach(row => total += row.positive);
+  arr.forEach(row => total += row[attr]);
   return total;
 };
 
