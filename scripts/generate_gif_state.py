d0096da0e88700586774dbf4dd2d7b347f1ca7b4
caldric <cvaldric@gmail.com>
2020-07-28 20:51:54 -0400

    Auto format using Black

--- scripts/generate_gif_state.py
+++ scripts/generate_gif_state.py
@@ -13,94 +13,125 @@ from plotly.io import write_image
 
 def main():
     # Establish color scale for choropleth map
-    colors = ['#fff0b0', '#edae4b', '#e3683c', '#bf2a48', '#6d2059', '#310958']
+    colors = ["#fff0b0", "#edae4b", "#e3683c", "#bf2a48", "#6d2059", "#310958"]
     bins = [0, 1_000, 5_000, 10_000, 20_000, 40_000]
     normalized_bins = list(map(lambda n: n / bins[-1], bins))
     color_scale = [[normalized_bins[i], colors[i]] for i in range(len(colors))]
 
     # Data variables
-    url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/' \
-          + 'csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+    url = (
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/"
+        + "csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+    )
     infected_data = wrangle_data(url)
-    dates = sorted(set(infected_data['date']))
+    dates = sorted(set(infected_data["date"]))
     infected_data_at = {}
     for date in dates:
-        infected_data_at[date] = infected_data.loc[infected_data['date'] == date].reset_index()
+        infected_data_at[date] = infected_data.loc[
+            infected_data["date"] == date
+        ].reset_index()
 
     # Create PNG files of the figure for each day and store in 'images/state_timeline' directory
-    if not os.path.exists('images/state_timeline'):
-        os.mkdir('images/state_timeline')
+    if not os.path.exists("images/state_timeline"):
+        os.mkdir("images/state_timeline")
 
     for i in range(len(dates)):
         data = infected_data_at[dates[i]]
-        path = join('images/state_timeline', '{}.png'.format(dates[i].strftime('%Y-%m-%d')))
-        total_cases = sum(data['infected_count'])
+        path = join(
+            "images/state_timeline", "{}.png".format(dates[i].strftime("%Y-%m-%d"))
+        )
+        total_cases = sum(data["infected_count"])
         if i > 0:
-            new_cases = total_cases - sum(infected_data_at[dates[i - 1]]['infected_count'])
+            new_cases = total_cases - sum(
+                infected_data_at[dates[i - 1]]["infected_count"]
+            )
         else:
             new_cases = 0
 
-        fig = go.Figure(data=go.Choropleth(
-            locations=data['state_code'],
-            z=data['infected_count'],
-            locationmode='USA-states',
-            zmin=0, zmax=bins[-1],
-            colorscale=color_scale,
-            autocolorscale=False,
-            marker_line_color='white'
-        ))
-        fig.update_layout(
-            title_text='<b>COVID-19 US Infected</b><br>Confirmed cases as of ' + dates[i].strftime('%B %d, %Y')
-            + '<br>Total cases: ' + '{:,}<br>New cases: {:,}'.format(total_cases, new_cases),
-            geo=dict(scope='usa')
+        fig = go.Figure(
+            data=go.Choropleth(
+                locations=data["state_code"],
+                z=data["infected_count"],
+                locationmode="USA-states",
+                zmin=0,
+                zmax=bins[-1],
+                colorscale=color_scale,
+                autocolorscale=False,
+                marker_line_color="white",
+            )
         )
-        write_image(fig, path, format='png')
+        fig.update_layout(
+            title_text="<b>COVID-19 US Infected</b><br>Confirmed cases as of "
+            + dates[i].strftime("%B %d, %Y")
+            + "<br>Total cases: "
+            + "{:,}<br>New cases: {:,}".format(total_cases, new_cases),
+            geo=dict(scope="usa"),
+        )
+        write_image(fig, path, format="png")
 
-    gif_writer('images/state_timeline')
+    gif_writer("images/state_timeline")
 
 
 def gif_writer(src, duration=None):
     """Combines images from src directory into one GIF"""
-    paths = sorted([join(src, file) for file in os.listdir(src) if file != '.DS_Store'])
+    paths = sorted([join(src, file) for file in os.listdir(src) if file != ".DS_Store"])
     gif = []
     for path in paths:
         gif.append(imageio.imread(path))
 
     if duration:
-        imageio.mimsave('images/state_timeline.gif', gif, duration=duration)
+        imageio.mimsave("images/state_timeline.gif", gif, duration=duration)
     else:
-        imageio.mimsave('images/state_timeline.gif', gif, duration=0.5)
+        imageio.mimsave("images/state_timeline.gif", gif, duration=0.5)
 
 
 def wrangle_data(url):
     # Lookup data
-    us_state_codes = pd.read_csv(join('data', 'us_state_codes.csv'))
-    us_state_coordinates = pd.read_csv(join('data', 'us_state_coordinates.csv'))
-    us_state_lookups = pd.merge(us_state_codes, us_state_coordinates, on=['state'], how='inner')
+    us_state_codes = pd.read_csv(join("data", "us_state_codes.csv"))
+    us_state_coordinates = pd.read_csv(join("data", "us_state_coordinates.csv"))
+    us_state_lookups = pd.merge(
+        us_state_codes, us_state_coordinates, on=["state"], how="inner"
+    )
 
     content = requests.get(url).content
-    raw_data = pd.read_csv(io.StringIO(content.decode('utf-8')))
+    raw_data = pd.read_csv(io.StringIO(content.decode("utf-8")))
 
     # Data wrangling
     # remove unneeded columns
     infected_data = raw_data.drop(
-        ['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Country_Region', 'Lat', 'Long_', 'Combined_Key'], axis=1
+        [
+            "UID",
+            "iso2",
+            "iso3",
+            "code3",
+            "FIPS",
+            "Admin2",
+            "Country_Region",
+            "Lat",
+            "Long_",
+            "Combined_Key",
+        ],
+        axis=1,
     )
     # rename state column
-    infected_data = infected_data.rename({'Province_State': 'state'}, axis=1)
+    infected_data = infected_data.rename({"Province_State": "state"}, axis=1)
     # group by state
-    infected_data = infected_data.groupby(['state']).sum().reset_index()
+    infected_data = infected_data.groupby(["state"]).sum().reset_index()
     # unpivot date columns
-    infected_data = infected_data.melt(id_vars=['state'], var_name='date', value_name='infected_count')
+    infected_data = infected_data.melt(
+        id_vars=["state"], var_name="date", value_name="infected_count"
+    )
     # transform date column from str to datetime
-    infected_data['date'] = pd.to_datetime(infected_data['date'], format='%m/%d/%y')
+    infected_data["date"] = pd.to_datetime(infected_data["date"], format="%m/%d/%y")
     # merge state codes and coordinates to infected data
-    infected_data = pd.merge(infected_data, us_state_lookups, on=['state'], how='right')
+    infected_data = pd.merge(infected_data, us_state_lookups, on=["state"], how="right")
     # rearrange columns
-    infected_data = infected_data[['state', 'state_code', 'latitude', 'longitude', 'date', 'infected_count']]
+    infected_data = infected_data[
+        ["state", "state_code", "latitude", "longitude", "date", "infected_count"]
+    ]
 
     return infected_data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- scripts/model.py
+++ scripts/model.py
@@ -12,10 +12,12 @@ from scipy.optimize import minimize
 
 def main():
     # Infected population
-    infected_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/' \
-          + 'csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+    infected_url = (
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/"
+        + "csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+    )
     infected_data = get_infected(infected_url)
-    infected_data = infected_data.groupby('date')['count'].sum()
+    infected_data = infected_data.groupby("date")["count"].sum()
     infected_data = infected_data.sort_index()
     infected_data = infected_data[-7:]
     start_date = infected_data.index[0]
@@ -25,16 +27,18 @@ def main():
     total_us_population = 328_239_523
 
     # Initial recovered population
-    recovered_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/' \
-        + 'csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
+    recovered_url = (
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/"
+        + "csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+    )
     recovered_data = get_recovered(recovered_url, start_date)
-    initial_recovered = recovered_data['count'].iloc[0]
+    initial_recovered = recovered_data["count"].iloc[0]
 
     # Intial population values
     initial = {
-        'recovered': initial_recovered,
-        'infected': infected_data[0],
-        'susceptible': total_us_population - infected_data[0] - initial_recovered
+        "recovered": initial_recovered,
+        "infected": infected_data[0],
+        "susceptible": total_us_population - infected_data[0] - initial_recovered,
     }
 
     # Input parameters
@@ -44,14 +48,22 @@ def main():
 
     # Solver
     estimated_infection_rate = solver(
-        func=objective, population=total_us_population, initial=initial, observed_infected=infected_data,
-        recovery_rate=recovery_rate, start_date=start_date
+        func=objective,
+        population=total_us_population,
+        initial=initial,
+        observed_infected=infected_data,
+        recovery_rate=recovery_rate,
+        start_date=start_date,
     )
 
     # Projection
     project(
-        population=total_us_population, initial=initial, days=400, infection_rate=estimated_infection_rate,
-        recovery_rate=recovery_rate, start_date=start_date
+        population=total_us_population,
+        initial=initial,
+        days=400,
+        infection_rate=estimated_infection_rate,
+        recovery_rate=recovery_rate,
+        start_date=start_date,
     )
 
 
@@ -75,10 +87,12 @@ def objective(infection_rate, population, initial, observed_infected, recovery_r
     t = np.array(range(len(observed_infected)))
 
     # Store initial conditions
-    initial_values = [initial['susceptible'], initial['infected'], initial['recovered']]
+    initial_values = [initial["susceptible"], initial["infected"], initial["recovered"]]
 
     # Solve ODE
-    compartments = odeint(sir_model, initial_values, t, args=(infection_rate, recovery_rate, population))
+    compartments = odeint(
+        sir_model, initial_values, t, args=(infection_rate, recovery_rate, population)
+    )
     infected = compartments[:, 1]
 
     observed = observed_infected
@@ -94,34 +108,47 @@ def solver(func, population, initial, observed_infected, recovery_rate, start_da
     # Solve for best fitting infection rate
     initial_infection_rate = 0.15
     solution = minimize(
-        func, initial_infection_rate, args=(population, initial, observed_infected, recovery_rate)
+        func,
+        initial_infection_rate,
+        args=(population, initial, observed_infected, recovery_rate),
     )
     estimated_infection_rate = solution.x[0]
 
     # Store initial conditions
-    initial_values = [initial['susceptible'], initial['infected'], initial['recovered']]
+    initial_values = [initial["susceptible"], initial["infected"], initial["recovered"]]
 
     # Solve ODE
-    compartments = odeint(sir_model, initial_values, t, args=(estimated_infection_rate, recovery_rate, population))
+    compartments = odeint(
+        sir_model,
+        initial_values,
+        t,
+        args=(estimated_infection_rate, recovery_rate, population),
+    )
     infected = compartments[:, 1]
     r0 = estimated_infection_rate / recovery_rate
 
     # Plot data
     fig1 = plt.figure()
     fig1.set_size_inches(10, 5)
-    plt.plot(t, infected, 'r-', label='estimated')
-    plt.plot(t, observed_infected, 'bo', label='observed')
-    plt.legend(loc='best')
-    plt.xlabel('time (days since {})'.format(start_date.strftime('%d %b %Y')))
-    plt.ylabel('infected count')
-    plt.title(r'Count vs. Time $(\beta={}, \gamma={}, R_0={}, r^2={})$'.format(
-        round(estimated_infection_rate, 4), round(recovery_rate, 4), round(r0, 1),
-        round(r_squared(observed_infected, infected), 4)
-    ))
+    plt.plot(t, infected, "r-", label="estimated")
+    plt.plot(t, observed_infected, "bo", label="observed")
+    plt.legend(loc="best")
+    plt.xlabel("time (days since {})".format(start_date.strftime("%d %b %Y")))
+    plt.ylabel("infected count")
+    plt.title(
+        r"Count vs. Time $(\beta={}, \gamma={}, R_0={}, r^2={})$".format(
+            round(estimated_infection_rate, 4),
+            round(recovery_rate, 4),
+            round(r0, 1),
+            round(r_squared(observed_infected, infected), 4),
+        )
+    )
 
     ax = plt.gca()
-    ax.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
-    plt.savefig('images/fit.png')
+    ax.get_yaxis().set_major_formatter(
+        plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x)))
+    )
+    plt.savefig("images/fit.png")
     plt.show()
     plt.close()
 
@@ -133,10 +160,12 @@ def project(population, initial, days, infection_rate, recovery_rate, start_date
     t = np.array(range(days))
 
     # Store initial conditions
-    initial_values = [initial['susceptible'], initial['infected'], initial['recovered']]
+    initial_values = [initial["susceptible"], initial["infected"], initial["recovered"]]
 
     # Solve ODE
-    compartments = odeint(sir_model, initial_values, t, args=(infection_rate, recovery_rate, population))
+    compartments = odeint(
+        sir_model, initial_values, t, args=(infection_rate, recovery_rate, population)
+    )
     susceptible = compartments[:, 0]
     infected = compartments[:, 1]
     recovered = compartments[:, 2]
@@ -147,61 +176,89 @@ def project(population, initial, days, infection_rate, recovery_rate, start_date
     fig2.set_size_inches(10, 5)
     ax = plt.gca()
 
-    plt.semilogy(t, susceptible, 'k-', label='susceptible')
-    plt.semilogy(t, infected, 'r-', label='infected')
-    plt.semilogy(t, recovered, 'b-', label='recovered')
+    plt.semilogy(t, susceptible, "k-", label="susceptible")
+    plt.semilogy(t, infected, "r-", label="infected")
+    plt.semilogy(t, recovered, "b-", label="recovered")
 
     # notable points
-    plt.semilogy(t[-1], susceptible[-1], 'ko')
-    ax.annotate('{:,}'.format(
-        int(round(susceptible[-1]))), (t[-1] - 20, 10 ** (np.log10(susceptible[-1]) + 0.15)), ha='center'
+    plt.semilogy(t[-1], susceptible[-1], "ko")
+    ax.annotate(
+        "{:,}".format(int(round(susceptible[-1]))),
+        (t[-1] - 20, 10 ** (np.log10(susceptible[-1]) + 0.15)),
+        ha="center",
     )
     t_max_infected = np.where(infected == max(infected))[0][0]
-    plt.semilogy(t_max_infected, max(infected), 'ro')
-    ax.annotate('{:,}'.format(int(round(max(infected)))), (t_max_infected, 10 ** (np.log10(max(infected)) - 0.50)), ha='center')
-    plt.semilogy(t[-1], infected[-1], 'ro')
-    ax.annotate('{:,}'.format(int(round(infected[-1]))), (t[-1], 10 ** (np.log10(infected[-1]) + 0.2)), ha='center')
+    plt.semilogy(t_max_infected, max(infected), "ro")
+    ax.annotate(
+        "{:,}".format(int(round(max(infected)))),
+        (t_max_infected, 10 ** (np.log10(max(infected)) - 0.50)),
+        ha="center",
+    )
+    plt.semilogy(t[-1], infected[-1], "ro")
+    ax.annotate(
+        "{:,}".format(int(round(infected[-1]))),
+        (t[-1], 10 ** (np.log10(infected[-1]) + 0.2)),
+        ha="center",
+    )
 
-    plt.legend(loc='best')
-    plt.xlabel('time (days since {})'.format(start_date.strftime('%d %b %Y')))
-    plt.ylabel('count')
-    plt.title(r'Count vs. Time $(\beta={}, \gamma={}, R_0={})$'.format(
-        round(infection_rate, 4), round(recovery_rate, 4), round(r0, 1)
-    ))
+    plt.legend(loc="best")
+    plt.xlabel("time (days since {})".format(start_date.strftime("%d %b %Y")))
+    plt.ylabel("count")
+    plt.title(
+        r"Count vs. Time $(\beta={}, \gamma={}, R_0={})$".format(
+            round(infection_rate, 4), round(recovery_rate, 4), round(r0, 1)
+        )
+    )
 
-    ax.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
-    plt.savefig('images/projection.png')
+    ax.get_yaxis().set_major_formatter(
+        plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x)))
+    )
+    plt.savefig("images/projection.png")
     plt.show()
     plt.close()
 
 
 def get_infected(url):
     # Lookup data
-    us_state_codes = pd.read_csv(join('data', 'us_state_codes.csv'))
-    us_state_coordinates = pd.read_csv(join('data', 'us_state_coordinates.csv'))
-    us_state_lookups = pd.merge(us_state_codes, us_state_coordinates, on=['state'], how='inner')
+    us_state_codes = pd.read_csv(join("data", "us_state_codes.csv"))
+    us_state_coordinates = pd.read_csv(join("data", "us_state_coordinates.csv"))
+    us_state_lookups = pd.merge(
+        us_state_codes, us_state_coordinates, on=["state"], how="inner"
+    )
 
     content = requests.get(url).content
-    raw_data = pd.read_csv(StringIO(content.decode('utf-8')))
+    raw_data = pd.read_csv(StringIO(content.decode("utf-8")))
 
     # Data wrangling
     # remove unneeded columns
     data = raw_data.drop(
-        ['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Country_Region', 'Lat', 'Long_', 'Combined_Key'], axis=1
+        [
+            "UID",
+            "iso2",
+            "iso3",
+            "code3",
+            "FIPS",
+            "Admin2",
+            "Country_Region",
+            "Lat",
+            "Long_",
+            "Combined_Key",
+        ],
+        axis=1,
     )
     # rename state column
-    data = data.rename({'Province_State': 'state'}, axis=1)
+    data = data.rename({"Province_State": "state"}, axis=1)
     # group by state
-    data = data.groupby(['state']).sum().reset_index()
+    data = data.groupby(["state"]).sum().reset_index()
     # unpivot date columns
-    data = data.melt(id_vars=['state'], var_name='date', value_name='count')
+    data = data.melt(id_vars=["state"], var_name="date", value_name="count")
     # transform date column from str to datetime
-    data['date'] = pd.to_datetime(data['date'], format='%m/%d/%y')
+    data["date"] = pd.to_datetime(data["date"], format="%m/%d/%y")
     # merge state codes and coordinates to infected data
-    data = pd.merge(data, us_state_lookups, on=['state'], how='right')
+    data = pd.merge(data, us_state_lookups, on=["state"], how="right")
     # rearrange columns
-    data = data[['state', 'state_code', 'latitude', 'longitude', 'date', 'count']]
-    data = data.sort_values(by='date')
+    data = data[["state", "state_code", "latitude", "longitude", "date", "count"]]
+    data = data.sort_values(by="date")
 
     return data
 
@@ -209,25 +266,25 @@ def get_infected(url):
 def get_recovered(url, start_date):
     # Obtain source data
     content = requests.get(url).content
-    raw_data = pd.read_csv(StringIO(content.decode('utf-8')))
+    raw_data = pd.read_csv(StringIO(content.decode("utf-8")))
 
     # Data wrangling
     # filter for country: US
-    data = raw_data[raw_data['Country/Region'] == 'US']
+    data = raw_data[raw_data["Country/Region"] == "US"]
     # remove unneeded rows
-    data = data.drop(['Province/State', 'Lat', 'Long'], axis=1)
+    data = data.drop(["Province/State", "Lat", "Long"], axis=1)
     # rename country column name
-    data = data.rename({'Country/Region': 'country'}, axis=1)
+    data = data.rename({"Country/Region": "country"}, axis=1)
     # unpivot data
-    data = data.melt(id_vars=['country'], var_name='date', value_name='count')
+    data = data.melt(id_vars=["country"], var_name="date", value_name="count")
     # transform date column to type datetime
-    data['date'] = pd.to_datetime(data['date'], format='%m/%d/%y')
+    data["date"] = pd.to_datetime(data["date"], format="%m/%d/%y")
     # filter only for the start_date
-    data = data[data['date'] >= start_date]
-    data = data.sort_values(by='date')
+    data = data[data["date"] >= start_date]
+    data = data.sort_values(by="date")
 
     return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
